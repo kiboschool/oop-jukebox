a90f1818be80c07b91e136027c2f9d38cbf05790
Rob Cobb <rwcobbjr@gmail.com>
2023-02-10 10:04:34 -0700

    update grade weights

--- test_jukebox.py
+++ test_jukebox.py
@@ -5,39 +5,39 @@ from jukebox import Jukebox
 songs = ["Kuna Kuna", "Herawa Ni", "Inauma", "Vaida", "Sasa Hivi", "McMca", "Dai Dai", "Woman", "Mbona", "Toto", "Kanairo dating", "Wanjapi"] 
 
 class TestJukebox(unittest.TestCase):
-    @weight(1)
+    @weight(10)
     def test_init_paused(self):
         player = Jukebox(songs)
         assert player.current_state() == "Paused"
 
-    @weight(2)
+    @weight(10)
     def test_play(self):
         player = Jukebox(songs)
         player.play()
         assert player.current_state() == "Playing: Kuna Kuna"
 
-    @weight(1)
+    @weight(10)
     def test_pause(self):
         player = Jukebox(songs)
         player.play()
         player.pause()
         assert player.current_state() == "Paused"
 
-    @weight(1)
+    @weight(10)
     def test_next(self):
         player = Jukebox(songs)
         player.play()
         player.next()
         assert player.current_state() == "Playing: Herawa Ni"
 
-    @weight(2)
+    @weight(10)
     def test_previous_wrap(self):
         player = Jukebox(songs)
         player.play()
         player.previous()
         assert player.current_state() == "Playing: Wanjapi"
 
-    @weight(2)
+    @weight(10)
     def test_next_next_prev(self):
         player = Jukebox(songs)
         player.play()
@@ -46,7 +46,7 @@ class TestJukebox(unittest.TestCase):
         player.previous()
         assert player.current_state() == "Playing: Herawa Ni"
 
-    @weight(3)
+    @weight(20)
     def test_next_wrap(self):
         player = Jukebox(songs)
         player.play()
